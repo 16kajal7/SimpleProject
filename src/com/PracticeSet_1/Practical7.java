d5377c1d7bac006d7e5b062bc7fc1754b7003aca
16kajal7@gmail.com <16kajal7@gmail.com>
2023-06-10 22:33:40 +0530

    Add Practical7 to SimpleProject

---
+++ src/com/PracticeSet_1/Practical7.java
@@ -0,0 +1,18 @@
+package com.PracticeSet_1;
+
+public class Practical7 {
+    public static void main(String[] args) {
+        
+        int n = 5;
+
+        for(int row = 1; row <= n; row++) {
+            //for every row run the column
+            for(int col = 1; col <= row; col++){
+                System.out.print("* ");
+            }
+           System.out.println();     
+        }
+        
+    }
+    
+}
