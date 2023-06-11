e9ddfcd0ccdb76ea2111babcf86f2f252b4eec02
16kajal7@gmail.com <16kajal7@gmail.com>
2023-06-11 20:15:58 +0530

    Add CharacterCount to StringsPractice

---
+++ src/com/StringsPractice/CharacterCount.java
@@ -0,0 +1,22 @@
+package com.StringsPractice;
+
+import java.lang.invoke.StringConcatFactory;
+import java.util.Scanner;
+
+public class CharacterCount {
+    public static void main(String[] args) {
+        // take input from user
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String inputString = scanner.nextLine();  // take the input until nextLine or string ends.
+
+        String stringWithoutSpaces = inputString.replaceAll("\\s+","");
+        
+        int characterCount = stringWithoutSpaces.length();  // store the input in 'inputString' until the string length.
+
+        System.out.println("Number of Characters: "+ characterCount);
+
+        scanner.close();
+    }  
+}
