827eb7490ddff911514ab246467ce9f99e7c2328
16kajal7@gmail.com <16kajal7@gmail.com>
2023-06-17 19:08:05 +0530

    Add CountVowels to StringsPractice

---
+++ src/com/StringsPractice/CountVowels.java
@@ -0,0 +1,22 @@
+package com.StringsPractice;
+
+public class CountVowels {
+    public static void main(String[] args) {
+        String str = "Hello World!";
+        int vowelCount = countVowels(str);
+        System.out.println("Number of Vowels: "+vowelCount);
+    }
+
+    public static int countVowels(String str) {
+        int count = 0;
+        str = str.toLowerCase();
+
+        for(int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                count++;
+            }
+        }
+        return count;
+    }   
+}
